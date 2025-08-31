078fb92ad10928256b0cca4c4d548bc84486ad4c
Yuji Ueki <unhappychoice@gmail.com>
2025-08-31 15:37:44 +0900

    feat: add gittype formula

---
+++ Formula/gittype.rb
@@ -0,0 +1,17 @@
+class Gittype < Formula
+  desc "A typing practice tool using your own code repositories"
+  homepage "https://github.com/unhappychoice/gittype"
+  url "https://github.com/unhappychoice/gittype/archive/v0.1.0.tar.gz"
+  sha256 "" # This will be updated automatically
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/gittype", "--version"
+  end
+end
