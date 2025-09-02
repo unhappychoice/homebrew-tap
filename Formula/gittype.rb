56f1561f1944940a5fb249639ae07172daed0756
Yuji Ueki <unhappychoice@gmail.com>
2025-09-02 16:34:42 +0900

    feat: add Linux ARM64 (aarch64) support for M1 compatibility

--- Formula/gittype.rb
+++ Formula/gittype.rb
@@ -20,6 +20,11 @@ class Gittype < Formula
       url "https://github.com/unhappychoice/gittype/releases/download/v0.2.0/gittype-v0.2.0-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "c1d2e3f4g5h6789012345678901234567890123456789012345678901234567890" # This will be updated automatically
     end
+
+    on_arm do
+      url "https://github.com/unhappychoice/gittype/releases/download/v0.2.0/gittype-v0.2.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "d1e2f3g4h5i6789012345678901234567890123456789012345678901234567890" # This will be updated automatically
+    end
   end
 
   def install
