abc87d12e16c5ba56aa296f3c2674e2c461c9573
GitHub Action <action@github.com>
2025-09-02 00:41:02 +0000

    chore: update gittype to v0.2.0

--- Formula/gittype.rb
+++ Formula/gittype.rb
@@ -1,8 +1,8 @@
 class Gittype < Formula
   desc "A typing practice tool using your own code repositories"
   homepage "https://github.com/unhappychoice/gittype"
-  url "https://github.com/unhappychoice/gittype/archive/v0.1.3.tar.gz"
-  sha256 "" # This will be updated automatically
+  url "https://github.com/unhappychoice/gittype/archive/v0.2.0.tar.gz"
+  sha256 "1a329344bfaee1b0a02cc6b8a82bbcc4c85a2c9625dfba02d89666e35ae2d3d4" # This will be updated automatically
   license "MIT"
 
   depends_on "rust" => :build
