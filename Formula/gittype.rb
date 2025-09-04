6e839b82a2a0e9873dc9641b7e9abbd405e6be84
GitHub Action <action@github.com>
2025-09-04 14:11:25 +0000

    chore: update gittype to v0.4.0

--- Formula/gittype.rb
+++ Formula/gittype.rb
@@ -5,8 +5,8 @@ class Gittype < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/unhappychoice/gittype/releases/download/v0.3.0/gittype-v0.3.0-x86_64-apple-darwin.tar.gz"
-      sha256 "3930d3eeba2103d84723fe851315b78ef61a3fdc3c9686aafbf570815130acc5"
+      url "https://github.com/unhappychoice/gittype/releases/download/v0.4.0/gittype-v0.4.0-x86_64-apple-darwin.tar.gz"
+      sha256 "0aa227ac0131405dd4bd6c7854e366b82070631ff146700f1b8e5f7c2bfcde86"
     end
 
     on_arm do
@@ -17,8 +17,8 @@ class Gittype < Formula
 
   on_linux do
     on_intel do
-      url "https://github.com/unhappychoice/gittype/releases/download/v0.3.0/gittype-v0.3.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "b9cfb71bb05dc9d5c566d78db01076e5d30e96074ec7f433e15e8c37a5d731a2"
+      url "https://github.com/unhappychoice/gittype/releases/download/v0.4.0/gittype-v0.4.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "5da3157e719c20498df5bbba4636b90cefdebf3732de8f4dfa43b49837f70d72"
     end
 
     on_arm do
