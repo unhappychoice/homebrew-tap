bc70155c751381eb8960806f51b8eeaa0e0d4302
GitHub Action <action@github.com>
2025-11-20 10:04:07 +0000

    chore: update gitlogue to v0.3.0

--- Formula/gitlogue.rb
+++ Formula/gitlogue.rb
@@ -5,25 +5,25 @@ class Gitlogue < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/unhappychoice/gitlogue/releases/download/v0.2.0/gitlogue-v0.2.0-x86_64-apple-darwin.tar.gz"
-      sha256 "f7d42caab07f09e76c1c761af94e0139f5581ba2e900a47f0a4311b8f2b4d4f5"
+      url "https://github.com/unhappychoice/gitlogue/releases/download/v0.3.0/gitlogue-v0.3.0-x86_64-apple-darwin.tar.gz"
+      sha256 "0c4a0dfe674f578ce10228810b45971e79ccfcd44ed560faf1dd5f8c291c9bae"
     end
 
     on_arm do
-      url "https://github.com/unhappychoice/gitlogue/releases/download/v0.2.0/gitlogue-v0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "c005a2db705ad865e8a664dae7b793e4b27c6fe51ce6cb645626adac3e7b41f9"
+      url "https://github.com/unhappychoice/gitlogue/releases/download/v0.3.0/gitlogue-v0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "b03761d9e3034d5a745196ba5c6762ffc8b5ebb58f35ed5aab1680644f025b2c"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/unhappychoice/gitlogue/releases/download/v0.2.0/gitlogue-v0.2.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "67fd769eabe54d19fc4a0067805283378f54017ab68b17427e4c7e2292ab8d78"
+      url "https://github.com/unhappychoice/gitlogue/releases/download/v0.3.0/gitlogue-v0.3.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "0a1975396898b36844c18a5e33dc093622cfa7efdd9bdf6baf04af2018fadd25"
     end
 
     on_arm do
-      url "https://github.com/unhappychoice/gitlogue/releases/download/v0.2.0/gitlogue-v0.2.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "20416861bf0f1501eaec6ad233bc3859a823602d13537e1a3862038d82daa2c1"
+      url "https://github.com/unhappychoice/gitlogue/releases/download/v0.3.0/gitlogue-v0.3.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "fab16a7646545203c3c79c0599df560ef0a0f3c449df2c03061c2671aa25b80b"
     end
   end
 
