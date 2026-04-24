f5730dddeb52bdeaedc7d37bc90d4a5032394641
GitHub Action <action@github.com>
2026-04-24 21:23:05 +0000

    chore: update splashboard to v0.2.0

--- Formula/splashboard.rb
+++ Formula/splashboard.rb
@@ -5,25 +5,25 @@ class Splashboard < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v0.0.1/splashboard-v0.0.1-x86_64-apple-darwin.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v0.2.0/splashboard-v0.2.0-x86_64-apple-darwin.tar.gz"
+      sha256 "7b4a7098f54a39fe84fcf551ecb11c54815d026eedc49757ce42ae7f46b34cd1"
     end
 
     on_arm do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v0.0.1/splashboard-v0.0.1-aarch64-apple-darwin.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v0.2.0/splashboard-v0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "c251adc3d5798b08729620869e7ecc20ee2bcc85dc500a03420d2ec8a485c765"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v0.0.1/splashboard-v0.0.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v0.2.0/splashboard-v0.2.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "a15d2db03cdf0832b51267b8f33c240ce3e35ab306aa725f709ccc009da594be"
     end
 
     on_arm do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v0.0.1/splashboard-v0.0.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v0.2.0/splashboard-v0.2.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "e4108c9daf91023461b9d9f6888377cce0d64ed40178040dc64dc1b2c51aeca1"
     end
   end
 
