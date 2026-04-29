8321e417af92a7cc0a46fa62932ff0674493d073
GitHub Action <action@github.com>
2026-04-29 01:08:56 +0000

    chore: update splashboard to v1.1.1

--- Formula/splashboard.rb
+++ Formula/splashboard.rb
@@ -5,25 +5,25 @@ class Splashboard < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v1.1.0/splashboard-v1.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "69732fca04f179f50f15e85a532fc14f2027fafe396c0f13ae00841edb94a339"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v1.1.1/splashboard-v1.1.1-x86_64-apple-darwin.tar.gz"
+      sha256 "8a6eccdf869a4b818096be87449facbf7028cf8a21b4770b5e21146b0cc44ddc"
     end
 
     on_arm do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v1.1.0/splashboard-v1.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "57d05054b71672c6b8a1f90bdcfa2beda60ddb2662f07978c5af97ecf587bbf3"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v1.1.1/splashboard-v1.1.1-aarch64-apple-darwin.tar.gz"
+      sha256 "a300eff1c460c0988a408da32e53d85ad2c76b7180f9fb5dd3e81d2ef532427b"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v1.1.0/splashboard-v1.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "b745a3c8eae0363a869901ef608ee4ef98f4b2759f855182f32d3734ba4bc21e"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v1.1.1/splashboard-v1.1.1-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "db6f53f6181f45424b607c1fc021e057852d60c74c17fb7106bb2d2750a3bd74"
     end
 
     on_arm do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v1.1.0/splashboard-v1.1.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "fa27370f483a3fb1b54df70e686a727431b3d02b931583826a63cf76a9b41c1b"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v1.1.1/splashboard-v1.1.1-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "3dd808ed9849dd4c2836ed3f490ee8b982e492953ebed9f7d1b109b03c39dbda"
     end
   end
 
