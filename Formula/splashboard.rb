62a4dcc786818aa468403783a37edd54daf20c3d
GitHub Action <action@github.com>
2026-05-06 15:40:38 +0000

    chore: update splashboard to v1.3.0

--- Formula/splashboard.rb
+++ Formula/splashboard.rb
@@ -5,25 +5,25 @@ class Splashboard < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v1.2.2/splashboard-v1.2.2-x86_64-apple-darwin.tar.gz"
-      sha256 "49a6c785996d6e8920e4abd41dee71af23f7b8ad3fe592c73d59b7038793e858"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v1.3.0/splashboard-v1.3.0-x86_64-apple-darwin.tar.gz"
+      sha256 "a3484e7d746a0d81f35db6642187abb53edf0b770f2476fe9efa908b26198ab4"
     end
 
     on_arm do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v1.2.2/splashboard-v1.2.2-aarch64-apple-darwin.tar.gz"
-      sha256 "7c8b52361e965af7319fb6f5abb45ef2b9f0b425877e03fe70984cb2e67650b5"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v1.3.0/splashboard-v1.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "7cd45334991684c3a88f4a30a3272b612d8b6b5e58de9b8cbe53dd2760c8cd7e"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v1.2.2/splashboard-v1.2.2-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "b430d17214dd39cda8826af819186a569688e15f1e854162a5f9f191859e0e8f"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v1.3.0/splashboard-v1.3.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "d87eddf78b2ea1bf81d953f46788afd1d602af978940125dc9f2990e6d08cf00"
     end
 
     on_arm do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v1.2.2/splashboard-v1.2.2-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "8ebbf34fa258d94c2917924dd29357bdcb8b53f9984492869f09075dbfd0e07f"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v1.3.0/splashboard-v1.3.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "53eedd26aee679ff81b83427835e7cfa57520cb42a912aa5cdb0b4329e231bd3"
     end
   end
 
