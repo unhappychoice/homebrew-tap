cffd43f4924cb3e24fd158ead8873bc4bfe40790
GitHub Action <action@github.com>
2026-05-07 13:15:01 +0000

    chore: update kobito to v0.3.0

--- Formula/kobito.rb
+++ Formula/kobito.rb
@@ -5,25 +5,25 @@ class Kobito < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/unhappychoice/kobito/releases/download/v0.2.0/kobito-v0.2.0-x86_64-apple-darwin.tar.gz"
-      sha256 "0e01fd16a9acacd6963a58eb1284e0d5fded8ce86463acd36bb296ebb1d5a75a"
+      url "https://github.com/unhappychoice/kobito/releases/download/v0.3.0/kobito-v0.3.0-x86_64-apple-darwin.tar.gz"
+      sha256 "2ac2eaf792b92bd7bfd4195cfde12f826381af241b251fa769d0b7431ce1b209"
     end
 
     on_arm do
-      url "https://github.com/unhappychoice/kobito/releases/download/v0.2.0/kobito-v0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "c3068e16ee19ab032fea105a0e8781f7af9462639691d44387e11375395b8dd3"
+      url "https://github.com/unhappychoice/kobito/releases/download/v0.3.0/kobito-v0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "028ec1fce1294b350d60c8252f2977169f49fe15ebca4d4095adb4ab893e6040"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/unhappychoice/kobito/releases/download/v0.2.0/kobito-v0.2.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "31c49632cb3ccb3ace2abb197d6c507d17eccadfbd4b950065b87bd87e35a42f"
+      url "https://github.com/unhappychoice/kobito/releases/download/v0.3.0/kobito-v0.3.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "255bb38e9322422c818b0997343aca1302db20073064816bc10c605421668098"
     end
 
     on_arm do
-      url "https://github.com/unhappychoice/kobito/releases/download/v0.2.0/kobito-v0.2.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "9c19e2f4e79c93a6b066ab54aa1f7d8463406bcc2070e7d75795bdc5ad2a4e76"
+      url "https://github.com/unhappychoice/kobito/releases/download/v0.3.0/kobito-v0.3.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "21fcb58f47331d7dbd6c9e6db89d8be07d5386371a7f7944c2075a4144e3b29d"
     end
   end
 
