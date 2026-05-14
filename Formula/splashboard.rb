c36a6c4c5500916de7bdf1cb95822b112acadb23
GitHub Action <action@github.com>
2026-05-14 11:51:22 +0000

    chore: update splashboard to v2.1.0

--- Formula/splashboard.rb
+++ Formula/splashboard.rb
@@ -5,25 +5,25 @@ class Splashboard < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v2.0.0/splashboard-v2.0.0-x86_64-apple-darwin.tar.gz"
-      sha256 "3e11acf1c3f6734daf718487b06039ecdb69cd4060a40ec031ea7c25f97cda7f"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v2.1.0/splashboard-v2.1.0-x86_64-apple-darwin.tar.gz"
+      sha256 "dc71b5c53b46e396297e3a5dfdf3c4795e9986a022a5ca2757edf9e2d46d1b91"
     end
 
     on_arm do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v2.0.0/splashboard-v2.0.0-aarch64-apple-darwin.tar.gz"
-      sha256 "eb61f3dc5e2597661874605e2b4538d8744595db0ea92ea1635a8b4120d1f684"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v2.1.0/splashboard-v2.1.0-aarch64-apple-darwin.tar.gz"
+      sha256 "d8b4e7e03394625619cfb92dc85e01c5e8a9c7bfe3cb0776f4ed37f1f0810465"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v2.0.0/splashboard-v2.0.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "b5d1707e60b61d6288d4f6d384d12f7b0e4a206517aa372d98badd2be9a56039"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v2.1.0/splashboard-v2.1.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "eb8b128171ac3dca856beb8a405248f3538b1e0f2de841e0e284c521e5fd0af7"
     end
 
     on_arm do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v2.0.0/splashboard-v2.0.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "9d8ed52ed729415af7ec626d82e4892eb2e91f0d1673e227174003766495ace6"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v2.1.0/splashboard-v2.1.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "f2c91885748f750f600404da127a6f883808d2897afcf991f4c8182cae284f80"
     end
   end
 
