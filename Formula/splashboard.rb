8e3fcca91ee699ec3a85944e15935280646f17d5
GitHub Action <action@github.com>
2026-05-18 13:31:33 +0000

    chore: update splashboard to v2.5.0

--- Formula/splashboard.rb
+++ Formula/splashboard.rb
@@ -5,25 +5,25 @@ class Splashboard < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v2.4.0/splashboard-v2.4.0-x86_64-apple-darwin.tar.gz"
-      sha256 "57b7f7f4a212eecb48ce458121f673b1c3ec6a7927c7a5d7df02fef8aa104d51"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v2.5.0/splashboard-v2.5.0-x86_64-apple-darwin.tar.gz"
+      sha256 "c057129dc5749ab5dd5d993f377d1e5bddbfa3140d948e3d197ac6e0f38d8fce"
     end
 
     on_arm do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v2.4.0/splashboard-v2.4.0-aarch64-apple-darwin.tar.gz"
-      sha256 "45e93863a060319a15744fc520f4dc013c61531f3c7249a6356df52a423ee306"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v2.5.0/splashboard-v2.5.0-aarch64-apple-darwin.tar.gz"
+      sha256 "fdaa9ee48a9b3398303eca1f3ed75e1c313de13bed8bf27803a589a8cad38c0c"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v2.4.0/splashboard-v2.4.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0ed1489345802806e98bb4d9a0f49b8991d176bcf9b59eea97e444361ba8c9da"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v2.5.0/splashboard-v2.5.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "b7f0d5f9e1c332bec2001fe45c1a815cf2b12b03cecc914ee73ad70e675e7e5d"
     end
 
     on_arm do
-      url "https://github.com/unhappychoice/splashboard/releases/download/v2.4.0/splashboard-v2.4.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "a02e41e361e271a0dd1789a6c2ccfc716dee390da1e35e49f794cf92f048dad0"
+      url "https://github.com/unhappychoice/splashboard/releases/download/v2.5.0/splashboard-v2.5.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "ef2d5c83bd4e986ed685972d366a5678f5efa38c25209790c178dada6f20403f"
     end
   end
 
